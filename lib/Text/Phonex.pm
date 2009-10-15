17d348c605bdcbeb27b029a1f358a19e7fa4e411
Korsani <korsani@free.fr>
2009-10-15 08:49:46 +0000

    Import of KORSANI/Text-Phonex-0.04 from CPAN.
    gitpan-cpan-distribution: Text-Phonex
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         KORSANI/Text-Phonex-0.04.tar.gz
    gitpan-cpan-author:       KORSANI
    gitpan-cpan-maturity:     released

--- lib/Text/Phonex.pm
+++ lib/Text/Phonex.pm
@@ -8,7 +8,7 @@ BEGIN {
 	our ($VERSION, @ISA, @EXPORT, @EXPORT_OK );
 	@ISA=qw(Exporter);
 	@EXPORT=qw( phonex );
-	$VERSION='0.03';
+	$VERSION='0.04';
 	sub VERSION {
 		(my $me, my $askedver)=@_;
 		$VERSION=~s/(.*)_\d+/$1/;
